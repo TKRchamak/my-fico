78cc25769ab2fcdef96c8a773ffc09f402ef8e11
noel.alam <noel.alam9999@gmail.com>
2023-09-26 16:22:46 +0600

    feat:basic server

---
+++ controllers/index.js
@@ -0,0 +1,12 @@
+const OrderModel = require("../models");
+const createOrder = async (req, res) => {
+  try {
+    res.send();
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+module.exports = {
+  createOrder,
+};
---
+++ routes/index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const router = require("express").Router();
+const bnpl = require("../controllers");
+
+// router.get("/insurance/:user_id", insurance.getInsuranceByUserId);
+// router.post("/insurance", insurance.createInsuranceWithShipmentId);
+// router.post("/insurance/calculate-premium", insurance.calculateInsurance);
+
+module.exports = router;
